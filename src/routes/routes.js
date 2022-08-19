cbfca1de84c974db46e94cb8b6c25bb47c5ad4d6
Munibulhassan <107879898+Munibulhassan@users.noreply.github.com>
2022-08-19 18:31:33 +0500

    add cloud database

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,16 +7,11 @@ const signature = require("./signature/signature");
 const subscription = require("./subscription/subscription");
 const team = require("./team/team");
 
-app.use("/auth",auth)
-app.use("/signature",signature)
-app.use("/subscription",subscription)
-app.use("/team",team)
-app.use("/document",documents)
-app.use("/folder",folder)
-
-
-
-
-
+app.use("/auth", auth);
+app.use("/signature", signature);
+app.use("/subscription", subscription);
+app.use("/team", team);
+app.use("/document", documents);
+app.use("/folder", folder);
 
 module.exports = app;
